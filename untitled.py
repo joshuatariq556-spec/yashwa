e5276744b8bc58cea45b427c57bf43a5513a3169
joshuatariq556-spec <joshuatariq556@gmail.com>
2025-09-06 02:14:58 -0700

    Add files via upload

---
+++ untitled.py
@@ -0,0 +1,29 @@
+#pip install streamlit
+
+import streamlit as pd
+
+page_by_img = """
+<style>
+[data-testid="stAppViewContainer"] {
+    background-image: url ("http://eskipaper.com/images/background-images-5.jpg");
+    background-size: cover;
+    background-repeta: on-repeta;
+    background-attachedment: fixed;
+}    
+
+[data-testid="stHeder"]{
+    background: rgba(0,0,0,0);
+}    
+
+[data-testid ="stToolbar"]{
+    right: 2rem;
+}
+</style>
+"""
+
+st.markdomn(page_by_img, unsafe_allow_html=True)
+
+
+
+
+tab1, tab2, tab3 = st.tabs(['Home','junior','senior'])
